d1113d71fce8540283cfb78e6634612e8c57ab48
shinhyesoo <chfhrl99@naver.com>
2025-02-09 19:33:48 +0900

    portfolio 작업중

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,36 +1,22 @@
+import { BrowserRouter } from "react-router-dom";
 import "../style/default.scss";
+import Navigation from "./Navigation.jsx"; // Navigation 컴포넌트 불러오기
 
-
-function Header() {
-    return (
-        <>
-            <header>
-                {/* <div className="cursor"></div> */}
-                <div className="header-wrap flex px16" style={{ position: "fixed", top: 0 }}>
-                    <div className="flex alignCenter justifyBetween" style={{ width: "100%" }}>
-                        <img className="w80" src="/public/images/header-logo.svg" alt="html"></img>
-                        <div className="header-btn">
-                            <button className=" flex alignCenter justifyCenter w45 h45 bgc-main-col" style={{ borderRadius: "100px" }}>
-                                <ul className="flexColumn gap5">
-                                    <li className="flex w25 h5 bgc-main-yellow" style={{ borderRadius: "10px" }}></li>
-                                    <li className="flex w25 h5 bgc-main-yellow" style={{ borderRadius: "10px" }}></li>
-                                    <li className="flex w25 h5 bgc-main-yellow" style={{ borderRadius: "10px" }}></li>
-                                </ul>
-                            </button>
-                            <navigator className="header-btn-wrap">
-                                <ul className="menu-nav">
-                                    <li><a href="#">HOME</a></li>
-                                    <li><a href="#">PROJECT</a></li>
-                                    <li><a href="#">ABOUT</a></li>
-                                    <li><a href="#">CONTACT</a></li>
-                                </ul>
-                            </navigator>
-                        </div>
-                    </div>
-                </div >
-            </header>
-        </>
-    )
-}
+const Header = () => {
+  return (
+    <header>
+    <BrowserRouter>
+      <div className="header-wrap flex px16" style={{ position: "fixed", top: 0 }}>
+        <div className="flex alignCenter justifyBetween" style={{ width: "100%" }}>
+          <div className="p10">
+            <p className="terry italic">TERRY</p>
+          </div>
+          <Navigation />
+        </div>
+      </div>
+    </BrowserRouter>
+    </header>
+  );
+};
 
 export default Header;
